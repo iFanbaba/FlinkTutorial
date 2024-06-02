86f26bb4c9386bc2cfaf9133e0ed42fb8da3d632
“syaap6” <meiyifan@immotors.com>
2024-06-02 09:45:07 +0800

    注释

--- src/main/java/com/atguigu/chatper09/StateTest.java
+++ src/main/java/com/atguigu/chatper09/StateTest.java
@@ -24,7 +24,7 @@ import org.apache.flink.util.Collector;
  */
 
 public class StateTest {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
@@ -39,15 +39,15 @@ public class StateTest {
                 );
 
         stream.keyBy(data -> data.user)
-                        .flatMap(new MyFlatMap())
-                                .print();
+                .flatMap(new MyFlatMap())
+                .print();
 
 
         env.execute();
     }
 
     // 实现自定义的FlatMapFunction，用于Keyed State测试
-    public static class MyFlatMap extends RichFlatMapFunction<Event, String>{
+    public static class MyFlatMap extends RichFlatMapFunction<Event, String> {
         // 定义状态
         ValueState<Event> myValueState;
         ListState<Event> myListState;
@@ -110,23 +110,29 @@ public class StateTest {
 
         @Override
         public void flatMap(Event value, Collector<String> out) throws Exception {
+
+            String key = value.user;
             // 访问和更新状态
-            System.out.println(myValueState.value());
             myValueState.update(value);
-            System.out.println( "my value: " + myValueState.value() );
+            System.out.println(key + ": value: " + myValueState.value());
 
             myListState.add(value);
+            int listValueCount = 0;
+            for (Event event : myListState.get()) {
+                listValueCount++;
+            }
+            System.out.println(key + ": list value: " + listValueCount);
 
-            myMapState.put(value.user, myMapState.get(value.user) == null? 1: myMapState.get(value.user) + 1);
-            System.out.println( "my map value: " + myMapState.get(value.user) );
+            myMapState.put(value.user, myMapState.get(value.user) == null ? 1 : myMapState.get(value.user) + 1);
+            System.out.println(key + ": map value: " + myMapState.get(value.user));
 
             myReducingState.add(value);
-            System.out.println( "my reducing value: " + myReducingState.get() );
+            System.out.println(key + ": reducing value: " + myReducingState.get());
 
             myAggregatingState.add(value);
-            System.out.println( "my agg value: " + myAggregatingState.get() );
+            System.out.println(key + ": agg value: " + myAggregatingState.get());
 
-            count ++;
+            count++;
             System.out.println("count: " + count);
         }
     }
