97a49d3fea64ba05edc8176f07ac2d40da28319e
“syaap6” <meiyifan@immotors.com>
2024-06-02 11:27:48 +0800

    注释

--- src/main/java/com/atguigu/chatper09/TwoStreamFullJoinExample.java
+++ src/main/java/com/atguigu/chatper09/TwoStreamFullJoinExample.java
@@ -2,10 +2,12 @@ package com.atguigu.chatper09;
 
 /**
  * Copyright (c) 2020-2030 尚硅谷 All Rights Reserved
- * <p>
- * Project:  FlinkTutorial
- * <p>
- * Created by  wushengran
+ * 9.2.3
+ * 2. 列表状态（ListState）
+ * 在 Flink SQL 中，支持两条流的全量 Join，语法如下：
+ * SELECT * FROM A INNER JOIN B WHERE A.id = B.id；
+ * 这样一条 SQL 语句要慎用，因为 Flink 会将 A 流和 B 流的所有数据都保存下来，然后进
+ * 行 Join。不过在这里我们可以用列表状态变量来实现一下这个 SQL 语句的功能。代码如下：
  */
 
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
